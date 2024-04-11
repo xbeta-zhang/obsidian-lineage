16e72aebb6a2b85e93346466f34837820ae12685
ycnmhd <33285087+ycnmhd@users.noreply.github.com>
2024-04-11 13:03:04 +0100

    feat: navigate to heading

--- src/view/components/container/column/components/group/components/card/components/content/content.svelte
+++ src/view/components/container/column/components/group/components/card/components/content/content.svelte
@@ -1,43 +1,32 @@
 <script lang="ts">
-	import { getPlugin, getView } from 'src/view/components/container/context';
-	import { ActiveStatus } from 'src/view/components/container/column/components/group/components/active-status.enum';
-	import { markdownPreviewAction } from 'src/view/actions/markdown-preview-action';
+    import { getView } from 'src/view/components/container/context';
+    import { ActiveStatus } from 'src/view/components/container/column/components/group/components/active-status.enum';
+    import { markdownPreviewAction } from 'src/view/actions/markdown-preview-action';
+    import {
+        handleClick
+    } from 'src/view/components/container/column/components/group/components/card/components/content/event-handlers/handle-links';
 
-	export let active: ActiveStatus | null;
+    export let active: ActiveStatus | null;
 
     export let content: string;
 
-	const plugin = getPlugin();
-	const view = getView()
-	const store = view.documentStore
-    // eslint-disable-next-line no-undef
-    const onClick = (e: MouseEvent) => {
-        // eslint-disable-next-line no-undef
-        if (e.target instanceof HTMLAnchorElement) {
-            if (e.target.hasClass('internal-link')) {
-                const link = e.target.dataset.href;
-                const path = store.getValue().file.path;
-                if (link && path) {
-                    plugin.app.workspace.openLinkText(link, path, 'split');
-                }
-            }
-        }
-    };
+    const view = getView();
+    const onClick = handleClick(view);
 </script>
 
 <div
     class={'preview-container markdown-preview-view markdown-preview-section'}
     on:click={onClick}
-	use:markdownPreviewAction={content}
+    use:markdownPreviewAction={content}
 ></div>
 
 <style>
     .preview-container {
         width: 100%;
-		min-height: var(--min-node-height);
-		max-height: 65vh;
-		font-size: var(--font-text-size);
-		padding: 6px 6px 6px 12px;
-		color-scheme: light
+        min-height: var(--min-node-height);
+        max-height: 65vh;
+        font-size: var(--font-text-size);
+        padding: 6px 6px 6px 12px;
+        color-scheme: light;
     }
 </style>
