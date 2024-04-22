2f8399c5fd130d2ed787a0dd2f4c635cc1307ed4
ycnmhd <33285087+ycnmhd@users.noreply.github.com>
2024-04-22 22:17:32 +0100

    tweak: disable merging while editing because of hotkey conflict with obsidian

--- src/view/actions/keyboard-shortcuts/helpers/commands/commands/merge-commands.ts
+++ src/view/actions/keyboard-shortcuts/helpers/commands/commands/merge-commands.ts
@@ -1,12 +1,12 @@
 import { mergeNode } from 'src/view/actions/keyboard-shortcuts/helpers/commands/commands/helpers/merge-node';
 import { PluginCommand } from 'src/view/actions/keyboard-shortcuts/helpers/commands/command-names';
-import { isActive } from 'src/view/actions/keyboard-shortcuts/helpers/commands/commands/helpers/is-editing';
+import { isActiveAndNotEditing } from 'src/view/actions/keyboard-shortcuts/helpers/commands/commands/helpers/is-editing';
 
 export const mergeCommands = () => {
     return [
         {
             name: 'merge_with_node_above',
-            check: isActive,
+            check: isActiveAndNotEditing,
             callback: (view) => {
                 mergeNode(view, 'up');
             },
@@ -17,7 +17,7 @@ export const mergeCommands = () => {
         },
         {
             name: 'merge_with_node_below',
-            check: isActive,
+            check: isActiveAndNotEditing,
             callback: (view) => {
                 mergeNode(view, 'down');
             },
